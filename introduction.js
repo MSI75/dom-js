6e59384dcc53031804e91afbd6989491e4025bc1
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2021-08-16 06:58:17 +0600

    first commit

---
+++ introduction.js
@@ -0,0 +1,9 @@
+// জাভাস্ক্রিপ্ট কি ? 
+/* জাভাস্ক্রিপ্ট হচ্ছে ্ওয়েব এপ্লিকেশন তৈরি করার জন্য সিঙ্গেল ক্লাইন্ড সাইট এপ্লিকেশন, যা সিঙ্গেল থেরেডের হাইলেভেল এবং ইন্টার প্যাটল প্রোগ্রামিং ল্যাংগয়েজ ।
+JavaScript (/ˈdʒɑːvəˌskrɪpt/)[9], often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.[10] JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.
+ */
+// প্রোগ্রামিং ল্যাংগয়েজ কি 
+
+
+/* কম্পিউটার ল্যাংগয়েজ কি 
+কম্পিউটার বুঝতে পারে এমন ভাষাকে কম্পিউটার ল্যাংগয়েজ বলে। যেমন ঃ [0,1] এর মাধ্যমে কম্পিউটার তার ভাষাকে বুঝতে পারে । */
